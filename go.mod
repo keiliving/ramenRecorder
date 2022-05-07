1a8a7381d63a7cd1317b3816ad61b19984b8f7c4
keiliving <keiliving6@gmail.com>
2022-05-07 15:26:10 +0900

    add frontend

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.18
 
 require (
 	cloud.google.com/go/storage v1.22.0
-	google.golang.org/api v0.78.0
+	google.golang.org/api v0.78.0 // indirect
 )
 
 require (
--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
-	"cloud.google.com/go/storage"
 	"github.com/joho/godotenv"
-	"github.com/keiliving/ramenRecorder/api"
-	"google.golang.org/api/option"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -18,18 +14,18 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	err := godotenv.Load(".env")
+	godotenv.Load(".env")
 	message := os.Getenv("SAMPLE_MESSAGE")
 	log.Println(message)
-	// http.HandleFunc("/", handler)
-	// http.ListenAndServe(":8080", nil)
-	credentialFilePath := "./key.json"
-	ctx := context.Background()
-	client, err := storage.NewClient(ctx, option.WithCredentialsFile(credentialFilePath))
-	if err != nil {
-		log.Fatal(err)
-	}
+	http.Handle("/", http.FileServer(http.Dir("frontend/build")))
+	http.ListenAndServe(":8080", nil)
+	// credentialFilePath := "./key.json"
+	// ctx := context.Background()
+	// client, err := storage.NewClient(ctx, option.WithCredentialsFile(credentialFilePath))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	user := &api.User{Client: client}
-	user.Upload(ctx)
+	// user := &api.User{Client: client}
+	// user.Upload(ctx)
 }
