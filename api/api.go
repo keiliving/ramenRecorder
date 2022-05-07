fb361388d278059570191f24ecab7cac44a0c59a
keiliving <keiliving6@gmail.com>
2022-05-07 13:57:21 +0900

    use modules

--- api/api.go
+++ api/api.go
@@ -9,21 +9,25 @@ import (
 	"cloud.google.com/go/storage"
 )
 
-type Client struct {
-	client *storage.Client
-	ctx    context.Context
+type a interface {
+	upload()
 }
 
-func (rc *Client) upload(ramenClient Client) {
+type User struct {
+	Client *storage.Client
+}
+
+func (user *User) Upload(ctx context.Context) {
 	// mock file
 	f, err := os.Create("sample.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	// オブジェクトのReaderを作成
 	bucketName := "ramen-recorder"           // e.g. example-bucket
 	objectPath := "sample-object/sample.txt" // e.g. foo/var/sample.txt
-	writer := rc.client.Bucket(bucketName).Object(objectPath).NewWriter(rc.ctx)
+	writer := user.Client.Bucket(bucketName).Object(objectPath).NewWriter(ctx)
 	if _, err := io.Copy(writer, f); err != nil {
 		panic(err)
 	}
@@ -31,4 +35,5 @@ func (rc *Client) upload(ramenClient Client) {
 	if err := writer.Close(); err != nil {
 		panic(err)
 	}
+	log.Println("upload succeeded!")
 }
